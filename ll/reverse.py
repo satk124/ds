edff32397f603eb89b6dc0da98cf8bda9ffefa9d
cloud infra <satk124@gmail.com>
2018-01-21 05:56:26 +0530

    linklist reverse

---
+++ ll/reverse.py
@@ -0,0 +1,29 @@
+import sys
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class ll:
+    def __init__(self):
+        self.head_ref = None
+
+    def rev(self, head):
+
+        if not head:
+            return head
+        if not head.next:
+            return head
+
+
+
+
+
+def main(args):
+    print 'start code'
+
+
+if __name__ == '__main__':
+    main(sys.argv)
